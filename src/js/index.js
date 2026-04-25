2dcbdf9135fca308b660b12cc56e3bb1af9b840b
bulatova_karina <midnight737475@gmail.com>
2026-04-26 00:18:23 +0400

    промежуточный коммит

--- src/js/index.js
+++ src/js/index.js
@@ -1,2 +1,27 @@
-import "./import/modules";
-import "./import/components";
+import Swiper from "swiper";
+import { Navigation } from "swiper/modules";
+
+const reviewsSlider = document.querySelector(".reviews__slider");
+
+if (reviewsSlider) {
+  new Swiper(reviewsSlider, {
+    modules: [Navigation],
+    slidesPerView: 2,
+    spaceBetween: 28,
+    speed: 600,
+    navigation: {
+      prevEl: ".reviews__prev",
+      nextEl: ".reviews__next",
+    },
+    breakpoints: {
+      0: {
+        slidesPerView: 1,
+        spaceBetween: 16,
+      },
+      768: {
+        slidesPerView: 2,
+        spaceBetween: 28,
+      },
+    },
+  });
+}
